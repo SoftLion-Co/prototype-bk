435a373290292503b4181d1febb40b4c3dee840e
Danyil Terentiev <96494594+DanyilTerentiev@users.noreply.github.com>
2023-07-10 18:25:27 +0300

    Base entity (#8)
    * create baseEntity model
    
    * create base entity
    
    ---------
    
    Co-authored-by: Lyzen Vira <133158996+LyzenVira@users.noreply.github.com>

---
+++ DAL/Entities/Base/BaseEntity.cs
@@ -0,0 +1,15 @@
+﻿namespace DAL.Entities.Base
+{
+    public abstract class BaseEntity
+    {
+        public Guid Id { get; set; }
+        public DateTime CreatedDateTime { get; set; } = DateTime.Now;
+        public DateTime? UpdatedDateTime { get; set; }
+        public BaseEntity()
+        {
+            Id = Guid.NewGuid();
+            CreatedDateTime = DateTime.Now;
+            UpdatedDateTime = null;
+        }
+    }
+}
