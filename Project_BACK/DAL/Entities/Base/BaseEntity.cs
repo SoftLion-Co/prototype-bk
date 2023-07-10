6564c01a8af51c3b7df2510cc467954760450460
Danyil Terentiev <dt210204@gmail.com>
2023-07-10 19:19:45 +0300

    base entity

---
+++ Project_BACK/DAL/Entities/Base/BaseEntity.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Entities.Base
+{
+    public abstract class BaseEntity
+    {
+        public Guid Id { get; set; }
+        public DateTime CreatedDateTime { get; set; }
+        public DateTime? UpdatedDateTime { get; set; }
+        public BaseEntity()
+        {
+            Id = Guid.NewGuid();
+            CreatedDateTime = DateTime.Now;
+            UpdatedDateTime = null;
+        }
+    }
+}
