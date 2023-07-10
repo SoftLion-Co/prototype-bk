7ffa0b8cb2f8e20554140c5b8a5aa9b83a167a2b
igorMaksimov <133158996+LyzenVira@users.noreply.github.com>
2023-07-10 18:40:59 +0300

    feature : create models

---
+++ Project_BACK/DAL/Entities/Picture.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Entities
+{
+    public class Picture
+    {
+        public byte[] Content { get; set; } = null!;
+
+    }
+}
