a8363e7219bb73d133cdfb13848bbf2f8abe81f0
Danyil Terentiev <dt210204@gmail.com>
2023-07-11 11:52:13 +0300

    create desc entity with config and repository

---
+++ DAL/Context/Configurations/BaseConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using DAL.Entities.Base;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Context.Configurations
+{
+    internal class BaseConfiguration<TEntity> : IEntityTypeConfiguration<TEntity> where TEntity : BaseEntity
+    {
+        public virtual void Configure(EntityTypeBuilder<TEntity> builder)
+        {
+            builder.HasKey(e => e.Id);
+            builder.Property(e => e.Id).HasColumnName("id");
+            builder.Property(e => e.CreatedDateTime).HasColumnName("createdDateTime").IsRequired();
+            builder.Property(e => e.UpdatedDateTime).HasColumnName("updatedDateTime");
+        }
+    }
+}
