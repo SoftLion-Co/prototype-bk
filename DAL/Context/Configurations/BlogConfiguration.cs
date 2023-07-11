29064500435d80483677b058ec89a0cfbaa53907
Danyil Terentiev <dt210204@gmail.com>
2023-07-11 19:59:51 +0300

    Blog add that is related with author.

---
+++ DAL/Context/Configurations/BlogConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DAL.Context.Configurations
+{
+    internal class BlogConfiguration : BaseConfiguration<Blog>
+    {
+        public override void Configure(EntityTypeBuilder<Blog> builder)
+        {
+            base.Configure(builder);
+            builder.ToTable("blog");
+            builder.Property(e => e.Title).HasMaxLength(30);
+        }
+    }
+}
