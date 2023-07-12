173f7fb8e8886d276f6f7714282b9a7ceae6a54e
igorMaksimov <133158996+LyzenVira@users.noreply.github.com>
2023-07-12 08:26:34 +0300

    feature: create GenericRepository and configure Swagger
    write GenericRepository and implement to CountryRepository. Configure Swagger in program.cs

---
+++ DAL/Entities/Response/ResponseEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Entities.ResponseEntity
+{
+    public class ResponseEntity<TResult>
+    {
+        public TResult? Result { get; set; }
+        public string Message { get; set; } = string.Empty;
+        public bool Success { get; set; } = true;
+    }
+}
