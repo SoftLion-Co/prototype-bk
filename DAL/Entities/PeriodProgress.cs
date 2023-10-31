c82e51aedd6ebbd3f61d618584aa60f2c36e61c8
Lyzen Vira <133158996+LyzenVira@users.noreply.github.com>
2023-10-31 14:12:11 +0200

    fix: change designer to design in PeriodProgress

--- DAL/Entities/PeriodProgress.cs
+++ DAL/Entities/PeriodProgress.cs
@@ -7,7 +7,7 @@ namespace DAL.Entities
         public OrderProjectStatus OrderProjectStatus { get; set; } = null!;
         public Guid OrderProjectStatusId { get; set; }
         public int NumberWeek { get; set; } = 0;
-        public int Designer { get; set; } = 0;
+        public int Design { get; set; } = 0;
         public int Development { get; set; } = 0;
         public int Security { get; set; } = 0;
 
